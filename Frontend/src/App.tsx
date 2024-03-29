6617cf86ae463c41ace8753e45044f51349aa5cc
unknown <lalitwagh2804@gmail.com>
2024-03-29 19:40:17 +0530

    Addeed the changes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { SignUp, Signin } from "./pages/Login"
+import { DashBored } from "./pages/Dashbored"
 
 function App() {
 
 
   return (
     <div>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignUp/>} ></Route>
+          <Route path="/signin" element={<Signin/>} ></Route>
+          <Route path="/dashbored" element={<DashBored/>} ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
