bc1c3003ca23a832997257683a7cd9fceacff265
unknown <lalitwagh2804@gmail.com>
2024-03-29 19:35:16 +0530

    Added changes

---
+++ Backend/src/Routes/user.ts
@@ -0,0 +1,13 @@
+import { Hono } from "hono";
+
+export const userRouter = new Hono<{
+    Bindings:{
+        DATABASE_URL:string;
+        JWT_SECRET:string;
+    }
+}>();
+
+userRouter.post('/signup' ,  async(c) =>{
+    
+})
+
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,9 +1,18 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { userRouter } from './Routes/user';
+import { eventRouter } from './Routes/event';
 
-const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL :string;
+    JWT_SECRET:string;
+  }
+}>();
+
+app.use("/api/*" , cors());
+app.route("api/v1/user" , userRouter);
+app.route("api/v1/user" , eventRouter);
 
 export default app
