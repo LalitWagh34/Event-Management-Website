20f7f429354c5fcdfe9239d30f400e0a16dd8e02
unknown <lalitwagh2804@gmail.com>
2024-03-29 19:38:23 +0530

    Added Login section

---
+++ Frontend/src/pages/Login.tsx
@@ -0,0 +1,15 @@
+import { Auth } from "../components/Auth"
+
+export const Signin =()=>{
+    return <div>
+        <Auth type="signin"/>
+
+    </div>
+}
+
+
+export const SignUp =()=>{
+    return <div>
+        <Auth type="signup"/>
+    </div>
+}
